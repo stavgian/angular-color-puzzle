338945b303d6eb5e9274d84d2b21a3c334f1692c
Yiannis Stavgianoudakis <stavgian@gmail.com>
2019-09-30 20:34:23 +0200

    Test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,37 +8,15 @@ import { Cell } from "./models/Cell";
 })
 export class AppComponent implements OnInit {
   title: string = "color-game";
-  size: number = 5;
   boardSize: number = 6;
-  startCoordinates: any;
-  activeColor: string;
   currentColor: string;
-  colors = [
-    {
-      rgb: "rgb(255, 50, 50)", //red
-      symbol: "❤️"
-    },
-    {
-      rgb: "rgb(24, 187, 255)", //blue
-      symbol: "🔷"
-    },
-    {
-      rgb: "rgb(255, 224, 17)", //yellow
-      symbol: "⭐️"
-    },
-    {
-      rgb: "rgb(51, 153, 68)", //green
-      symbol: "🍀"
-    }
-  ];
-  queue = [];
+  colors = [];
   board = [];
   clicks: number = 0;
-  testBoard = [];
   solved = false;
 
   constructor() {
-    this.startCoordinates = [0, 0];
+    this.getColors();
     this.generateGameBoard();
   }
 
@@ -48,6 +26,22 @@ export class AppComponent implements OnInit {
     console.log(this.board);
   }
 
+  getColors() {
+    this.colors = [
+      {
+        rgb: "rgb(255, 50, 50)" //red
+      },
+      {
+        rgb: "rgb(24, 187, 255)" //blue
+      },
+      {
+        rgb: "rgb(255, 224, 17)" //yellow
+      },
+      {
+        rgb: "rgb(51, 153, 68)" //green
+      }
+    ];
+  }
   generateGameBoard() {
     const x = this.boardSize;
     const board = [];
@@ -183,154 +177,4 @@ export class AppComponent implements OnInit {
   randomColor() {
     return this.colors[Math.floor(Math.random() * this.colors.length)];
   }
-
-  onClickHandler(cell) {
-    console.log(cell);
-    this.board[0][0].color = cell.color;
-    this.queue.map(e => {
-      this.board[e.cell.x][e.cell.y].color = cell.color;
-    });
-    this.board[cell.x][cell.y].value = "Goal";
-    this.activeColor = this.board[cell.x][cell.y].color;
-    let path = this.findShortestPath(cell);
-
-    setTimeout(() => {
-      this.board[cell.x][cell.y].value = "Empty";
-    }, 200);
-
-    console.log("Queue", this.queue);
-    console.log("Path", path);
-  }
-
-  findShortestPath(cell) {
-    let distanceFromTop = this.startCoordinates[0];
-    let distanceFromLeft = this.startCoordinates[1];
-    let location = {
-      distanceFromTop: distanceFromTop,
-      distanceFromLeft: distanceFromLeft,
-      path: [],
-      status: "Start"
-    };
-
-    let queue = [location];
-
-    // Loop through the grid searching for the goal
-    while (queue.length > 0) {
-      // Take the first location off the queue
-      let currentLocation = queue.shift();
-
-      // Explore North
-      let newLocation = this.exploreInDirection(currentLocation, "North", cell);
-
-      if (newLocation.status === "Goal") {
-        return newLocation.path;
-      } else if (newLocation.status === "Valid") {
-        queue.push(newLocation);
-      }
-
-      // Explore East
-      newLocation = this.exploreInDirection(currentLocation, "East", cell);
-
-      if (newLocation.status === "Goal") {
-        this.queue.push(newLocation);
-        return newLocation.path;
-      } else if (newLocation.status === "Valid") {
-        this.queue.push(newLocation);
-        queue.push(newLocation);
-      }
-
-      // Explore South
-      newLocation = this.exploreInDirection(currentLocation, "South", cell);
-      if (newLocation.status === "Goal") {
-        this.queue.push(newLocation);
-        return newLocation.path;
-      } else if (newLocation.status === "Valid") {
-        this.queue.push(newLocation);
-        queue.push(newLocation);
-      }
-
-      // Explore West
-      newLocation = this.exploreInDirection(currentLocation, "West", cell);
-      if (newLocation.status === "Goal") {
-        this.queue.push(newLocation);
-        return newLocation.path;
-      } else if (newLocation.status === "Valid") {
-        this.queue.push(newLocation);
-        queue.push(newLocation);
-      }
-    }
-
-    console.log("Queue:", queue);
-    // No valid path found
-    return false;
-  }
-
-  exploreInDirection(currentLocation, direction, cell) {
-    var newPath = currentLocation.path.slice();
-    newPath.push(direction);
-
-    var dft = currentLocation.distanceFromTop;
-    var dfl = currentLocation.distanceFromLeft;
-
-    if (direction === "North") {
-      dft -= 1;
-    } else if (direction === "East") {
-      dfl += 1;
-    } else if (direction === "South") {
-      dft += 1;
-    } else if (direction === "West") {
-      dfl -= 1;
-    }
-
-    var newLocation = {
-      distanceFromTop: dft,
-      distanceFromLeft: dfl,
-      path: newPath,
-      cell: cell,
-      status: "Unknown"
-    };
-    newLocation.status = this.locationStatus(newLocation);
-
-    // If this new location is valid, mark it as 'Visited'
-    if (newLocation.status === "Valid") {
-      this.board[newLocation.distanceFromTop][
-        newLocation.distanceFromLeft
-      ].value = "Visited";
-      this.board[newLocation.distanceFromTop][
-        newLocation.distanceFromLeft
-      ].color = this.activeColor;
-    }
-
-    return newLocation;
-  }
-
-  // This function will check a location's status
-  // (a location is "valid" if it is on the grid, is not an "obstacle",
-  // and has not yet been visited by our algorithm)
-  // Returns "Valid", "Invalid", "Blocked", or "Goal"
-  locationStatus = function(location) {
-    var gridSize = this.board.length;
-    var dft = location.distanceFromTop;
-    var dfl = location.distanceFromLeft;
-
-    if (
-      location.distanceFromLeft < 0 ||
-      location.distanceFromLeft >= gridSize ||
-      location.distanceFromTop < 0 ||
-      location.distanceFromTop >= gridSize
-    ) {
-      // location is not on the grid--return false
-      return "Invalid";
-    } else if (this.board[dft][dfl].value === "Goal") {
-      return "Goal";
-    } else if (this.board[dft][dfl].color === this.activeColor) {
-      // location is either an obstacle or has been visited
-      return "Valid";
-    } else if (this.board[dft][dfl].value !== "Empty") {
-      // location is either an obstacle or has been visited
-      return "Blocked";
-    } else {
-      return "Valid";
-    }
-  };
 }
